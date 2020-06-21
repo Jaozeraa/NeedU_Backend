c07c95ff76b0f5c7a6fbfc941394081a2de93b16
Jaozeraa <joaog.beccari@gmail.com>
2020-06-20 21:11:24 -0300

    cors

--- src/server.ts
+++ src/server.ts
@@ -15,13 +15,12 @@ var corsOptions = {
   optionsSuccessStatus: 200
 }
 
-app.use(cors(corsOptions));
 
 app.use(express.json());
 
 app.use(routes);
 
-app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
+app.use('/uploads', cors(corsOptions), express.static(path.resolve(__dirname, '..', 'uploads')));
 
 const configMigrations: MigratorConfig = {
   directory: config.migrations.directory,
